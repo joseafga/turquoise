3172e0685d82f9a76af24f83f9550eb6e6d455a3
José Almeida <jose.afga@gmail.com>
2023-05-25 20:07:09 -0300

    Improved: commands messages

--- src/turquoise/commands/about.cr
+++ src/turquoise/commands/about.cr
@@ -2,7 +2,7 @@ Turquoise::Bot.command do |bot|
   cmd = Tourmaline::CommandHandler.new("sobre") do |ctx|
     text = "Olá, eu sou a *Turquesa*, um bot do Telegram super antenada nas últimas \
       novidades do YouTube! Além disso, adoro me divertir com outras atividades também! \
-      💙💚💎\n\n\
+      🥰\n\n\
       Conheça também meu [repositório no GitHub](https://github.com/joseafga/turquoise)."
     
     ctx.reply(text, disable_web_page_preview: true)
--- src/turquoise/commands/start.cr
+++ src/turquoise/commands/start.cr
@@ -1,10 +1,12 @@
 Turquoise::Bot.command do |bot|
   cmd = Tourmaline::CommandHandler.new(["help", "start"]) do |ctx|
-    markup = Tourmaline::Client.build_reply_keyboard_markup do |kb|
-      kb.button "/gato"
-      kb.button "/cachorro"
+    markup = Tourmaline::Client.build_reply_keyboard_markup(columns: 2) do |kb|
+      kb.button "/sobre"
+      kb.button "/gato 😺"
+      kb.button "/cachorro 🐶"
+      kb.button "/jogo"
     end
-    ctx.reply("😺 Use commands: /gato, /cachorro and /sobre", reply_markup: markup)
+    ctx.reply("Experimente os comandos: /gato, /cachorro ou /sobre", reply_markup: markup)
   end
 
   bot.register cmd
